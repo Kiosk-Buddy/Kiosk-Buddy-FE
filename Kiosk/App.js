9b43b97e4e806a2a53721a8f217688cf320af4e9
tjdrms2468 <165782028+tjdrms2468@users.noreply.github.com>
2025-05-01 21:30:08 +0900

    폴더 이름 변경

--- Kiosk/App.js
+++ Kiosk/App.js
@@ -1,21 +1,35 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import WaitingScreen from './screens/WaitingScreen';
-import SignupScreen from './screens/SignupScreen';
+import WarningScreen from './screens/WarningScreen';
+import KioskSelectScreen from './screens/KioskSelectScreen';
+import FoodSelectScreen from './screens/FoodSelectScreen';
+import ChooseSetOrSingleScreen from './screens/ChooseSetOrSingleScreen';
+import SelectSetDetailsScreen from './screens/SelectSetDetailsScreen';
+import CartScreen from './screens/CartScreen';
+import PaymentScreen from './screens/PaymentScreen';
+
+import { CartProvider } from './contexts/CartContext';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Waiting">
-        <Stack.Screen name="Waiting" component={WaitingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Warning" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Warning" component={WarningScreen} />
+          <Stack.Screen name="KioskSelect" component={KioskSelectScreen} />
+          <Stack.Screen name="FoodSelect" component={FoodSelectScreen} />
+          <Stack.Screen name="ChooseSetOrSingle" component={ChooseSetOrSingleScreen} />
+          <Stack.Screen name="SelectSetDetails" component={SelectSetDetailsScreen} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+          <Stack.Screen name="Payment" component={PaymentScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
 
